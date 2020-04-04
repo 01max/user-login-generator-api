92a4206d720e9decf83c1632176ed65f8669997a
Maxime Louguet <m.louguet@gmail.com>
2020-04-04 16:40:18 +0200

    Add User validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :login, format: { with: /\A[A-Z]{3}\z/, message: 'must be composed of exactly 3 capital letters' },
+                    uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,32 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:existing_user) { FactoryBot.create(:user) }
+
+  describe 'login regexp validation' do
+    let(:regexp_validation_message) { 'must be composed of exactly 3 capital letters' }
+    context 'when a new User has 3 capital letters as his login' do
+      subject(:invalid_user_login_regexp_errors) do
+        invalid_user = described_class.new(login: 'AAA')
+        invalid_user.valid?
+        invalid_user&.errors&.messages
+      end
+
+      it 'does not have this specific login error upon validation' do
+        expect(invalid_user_login_regexp_errors.dig(:login)).not_to include(regexp_validation_message)
+      end
+    end
+
+    context 'when a new User does not have 3 capital letters as his login' do
+      subject(:invalid_user_login_regexp_errors) do
+        invalid_user = described_class.new(login: 'invalid_login')
+        invalid_user.valid?
+        invalid_user&.errors&.messages
+      end
+
+      it 'has the expected login error upon validation' do
+        expect(invalid_user_login_regexp_errors.keys).to include(:login)
+        expect(invalid_user_login_regexp_errors[:login]).to include(regexp_validation_message)
+      end
+    end
+  end
 end
